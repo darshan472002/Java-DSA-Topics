45227cba22ab4e1165b1c9b3d13445eee075ee07
Darshan Prabhudas Panchal <91986412+darshan472002@users.noreply.github.com>
2023-07-30 16:40:08 +0530

    Better code for Insetion Sort

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -7,19 +7,32 @@ class InsertionSort {
 		}
 		System.out.println();
 	}
+
 	public static void main(String[] args) {
-		int arr[] = {7,8,3,1,2};
-		
+		int arr[] = { 7, 8, 3, 1, 2 };
+		int n = arr.length;
+
 		//Insertion Sort
-		for (int i=1; i<arr.length; i++) {
-			int current = arr[i];
-			int j = i-1;
-			while(j >= 0 && current < arr[j]) {
-				arr[j+1] = arr[j];
+		
+		// for (int i=1; i<n; i++) {
+		// 	int current = arr[i];
+		// 	int j = i-1;
+		// 	while(j >= 0 && current < arr[j]) {
+		// 		arr[j+1] = arr[j];
+		// 		j--;
+		// 	}
+		// 	//Placement
+		// 	arr[j+1] = current;
+		// }
+
+		for (int i = 0; i <= n - 1; i++) {
+			int j = i;
+			while (j > 0 && arr[j-1] > arr[j]) {
+				int temp = arr[j - 1];
+				arr[j - 1] = arr[j];
+				arr[j] = temp;
 				j--;
 			}
-			//Placement
-			arr[j+1] = current;
 		}
 		printArray(arr);
 	}
